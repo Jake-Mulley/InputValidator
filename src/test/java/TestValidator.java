bbf78176c500d56afcaeb86496ed246b0edee312
Jake-Mulley <jakemulley035@gmail.com>
2021-12-01 18:26:32 +0000

    created first test (testForAt) which tests if function emailFormCheck checks for inclusion of @ in given email (failing test)

--- src/test/java/TestValidator.java
+++ src/test/java/TestValidator.java
@@ -1,7 +1,25 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import cs3318.inputvalidator.Main;
+import cs3318.inputvalidator.Validator;
 
+/**
+ * Test class for the testing of Validator
+ */
 public class TestValidator {
+    /**
+     * Test that it checks for a @ in a given string, returns true if @ is present in string
+     *
+     */
+    @Test
+    public void testForAt() {
+        // validator instance to test
+        Validator testValidator = new Validator("", "");
+        testValidator.setEmail("contains@inString");
+        assertEquals(true, testValidator.emailFormChecker());
+        testValidator.setEmail("containsNoAt");
+        assertEquals(false, testValidator.emailFormChecker());
+    }
 }
 
 
